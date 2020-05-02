c261cd4d8f6a1b33bf0ce4d008a1f92c67f18ccb
satyajit <satyajit@clevertap.com>
2020-05-02 13:53:25 +0530

    Add example of provides with dependency

--- src/main/java/providessingleton/Main.java
+++ src/main/java/providessingleton/Main.java
@@ -8,7 +8,12 @@ public class Main {
     public static void main(String[] args) {
 
         Injector injector = Guice.createInjector(new Module());
-        SquareRequest square = injector.getInstance(SquareRequest.class);
-        square.makeRequest();
+        SquareRequest square1 = injector.getInstance(SquareRequest.class);
+        square1.makeRequest();
+
+        SquareRequest square2 = injector.getInstance(SquareRequest.class);
+        square2.makeRequest();
+
+        System.out.println(square1.getDrawShape() == square2.getDrawShape());
     }
 }
--- src/main/java/providessingleton/Module.java
+++ src/main/java/providessingleton/Module.java
@@ -3,12 +3,21 @@ package providessingleton;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.name.Names;
+import javax.inject.Named;
 
 public class Module extends AbstractModule {
 
+    @Override
+    protected void configure() {
+        bind(String.class).annotatedWith(Names.named("Color")).toInstance("Blue");
+        bind(Integer.class).annotatedWith(Names.named("Edge")).toInstance(50);
+
+    }
+
     @Provides
     @Singleton
-    public DrawShape providesDrawSquare() {
-        return new DrawSquare("Blue", 50);
+    public DrawShape providesDrawSquare(@Named("Color") String color, @Named("Edge") Integer edge) {
+        return new DrawSquare(color, edge);
     }
 }
